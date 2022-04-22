0e610916bc0efdac6fd525ceb1a84e6b861a66e9
SFelbeck <stefanfelbeck@gmail.com>
2022-04-21 22:16:01 -0700

    feature complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// links the inquirer and fs modules
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// Uses the inquirer module to ask the user for multiple inputs in the integrated terminal and stores them within objects
 inquirer
     .prompt([
         {
@@ -18,11 +20,11 @@ inquirer
             name: "installation",
             message: "Please enter some Installation Instructions"
         },
-        // {
-        //     type: 'list',
-        //     name: "liscense",
-        //     message: "What license does this application use?"
-        // },
+        {
+            type: 'input',
+            name: "license",
+            message: "What license does this application use? (lowercase only please)"
+        },
         {
             type: 'input',
             name: 'github',
@@ -51,13 +53,47 @@ inquirer
     ])
     .then((data) => {
         console.log(data);
+        //logs all of the md code in one large string as a const variable with calls to the earlier objects for the given answers, also lacks indentation since it broke the code
+        const readMe = `## Title: ${data.title}
+![](https://img.shields.io/badge/license-${data.license}-green)
+[${data.license}](https://choosealicense.com/licenses/${data.license})
+        
+## Table of Contents
 
-        let readMe = `Description: ${data.description} 
-        Installation Instructions: ${data.installation} 
-        Contributing: ${data.contributing} 
-        Questions: If you have any questions you can reach me at ${data.github} or at my email: ${data.email}.`
+--[Description](#description)
 
-        fs.writeFile(`${data.title}.md`, mdFile, (err) =>
+--[Usage](#usage)
+
+--[Installation](#installation)
+
+--[Testing](#testing)
+
+--[Contributing](#contributing)
+
+--[Questions](#questions)
+
+
+## Description: 
+${data.description} 
+
+## Usage:
+${data.usage}
+
+## Installation: 
+${data.installation} 
+
+## Testing:
+${data.tests}
+
+## Contributing: 
+${data.contributing} 
+
+## Questions: 
+If you have any questions you can reach me at:
+[GitHub](https://www.github.com/${data.github}) or at [email](${data.email}).`
+
+// Uses the fs module to generate a md file with the user's input at the title, also checks for errors and console logs if an error or success
+        fs.writeFile(`${data.title}.md`, readMe, (err) =>
         err ? console.log(err) : console.log('Success!')
       );
     });
